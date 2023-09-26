2c96fa1ed0b85dcec06d95b20f29faae84fff360
7uppp <markwang9315@gmail.com>
2023-09-26 14:07:50 +1000

    add comment to refreshTokenController

--- src/app/controllers/refreshTokenController.ts
+++ src/app/controllers/refreshTokenController.ts
@@ -4,13 +4,16 @@ import jwt from 'jsonwebtoken';
 export const refreshToken = (req: Request, res: Response) => {
     const refreshToken = req.headers['x-refresh-token'] as string;
 
+    //check if refreshToken is exit
     if (!refreshToken) return res.status(401).json({message: 'Refresh token not found'});
 
+    //check if refreshToken is valid
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, (err, decoded) => {
         if (err) return res.status(401).json({message: 'Invalid refresh token'});
 
         if (!decoded || !(decoded as any).userId) return res.status(401).json({message: 'Invalid token payload'});
 
+        //create new accessToken
         const accessToken = jwt.sign(
             {userId: (decoded as any).userId},
             process.env.ACCESS_TOKEN_SECRET as string,
